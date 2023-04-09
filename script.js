a56436dcee747e68419597142ea6547bcaba6238
SAHITYA INDORIYA <95152140+Sahitya0001@users.noreply.github.com>
2023-04-09 19:26:02 +0530

    uploading my all files

---
+++ script.js
@@ -0,0 +1,33 @@
+let text = document.getElementById("Text");
+let author = document.getElementById("author");
+let button = document.querySelector("#generator");
+let sound = document.getElementById("volume");
+let copy = document.getElementById("copy");
+let twitter = document.getElementById("twitter");
+
+function randomQuote() {
+    button.innerText = "Loading Quote...";
+    fetch("http://api.quotable.io/random").then(res => res.json()).then(result => {
+        text.innerText = result.content;
+        author.innerText = "-- " + result.author;
+        button.innerText = "New Quote";
+    });
+}
+
+button.addEventListener("click",randomQuote);
+
+sound.addEventListener("click",() => {
+    // let string = author.innerText;
+    // let str = string.substring()
+    let utterance = new SpeechSynthesisUtterance(`${text.innerText} by ${author.innerText}`);
+    speechSynthesis.speak(utterance);
+});
+
+copy.addEventListener("click",() => {
+    navigator.clipboard.writeText(text.innerText);
+});
+
+twitter.addEventListener("click",() => {
+    let tweetUrl = `https://twitter.com/intent/tweet?url=${text.innerText}`;
+    window.open(tweetUrl,"_blank");
+});
